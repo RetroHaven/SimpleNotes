577653e8365199b64019f3d68334a6f64b0532ae
eleanordoesntcode <166862894+eleanordoesntcode@users.noreply.github.com>
2025-04-19 02:27:36 +0200

    no, we do not greet the player.

--- src/main/java/org/eleanorsilly/mc/notes/TemplateListener.java
+++ src/main/java/org/eleanorsilly/mc/notes/TemplateListener.java
@@ -14,15 +14,4 @@ public class TemplateListener implements Listener {
         this.plugin = plugin;
         this.config = plugin.getConfig();
     }
-
-    // Handle player join event
-    @EventHandler(priority = Event.Priority.Normal)
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        // Example action: Send a welcome message configured in the plugin's configuration file to the player when they join the server
-
-        String welcomeMessage = config.getConfigString("settings.welcome-message.value");
-        welcomeMessage = welcomeMessage.replace("%player%", event.getPlayer().getName());
-
-        event.getPlayer().sendMessage(welcomeMessage);
-    }
 }
